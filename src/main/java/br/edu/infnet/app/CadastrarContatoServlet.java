808ebfa30e699a2a7a9b2e4c12d491c5b349910d
jennifer.omena <jennifer.omena@outlook.com>
2022-08-22 21:36:32 -0300

    continuação

--- src/main/java/br/edu/infnet/app/CadastrarContatoServlet.java
+++ src/main/java/br/edu/infnet/app/CadastrarContatoServlet.java
@@ -1,49 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.infnet.app;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringUtils;
 
-/**
- *
- * @author jennifer.omena
- */
 @WebServlet(name = "CadastrarContatoServlet", urlPatterns = {"/CadastrarContatoServlet"})
 public class CadastrarContatoServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CadastrarContatoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CadastrarContatoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String fone = request.getParameter("fone");
+        ArrayList<String> erros = new ArrayList<>();
+        if(StringUtils.isBlank(nome)) {
+            erros.add("O campo NOME é obrigatório");
         }
+        if(StringUtils.isBlank(email)) {
+            erros.add("O campo EMAIL é obrigatório");
+        }
+        if(StringUtils.isBlank(fone)) {
+            erros.add("O campo TELEFONE é obrigatório");
+        }
+        if(erros.isEmpty()) {
+            request.setAttribute("Sucesso", "O contato foi cadastrado com sucesso");
+        } else {
+            request.setAttribute("erros", erros);
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
